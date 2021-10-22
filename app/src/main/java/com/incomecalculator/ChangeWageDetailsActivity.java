8064f7afb69895166e48803c911a2d86ff7c8e39
Reka Kulcsar <reka.kulcsar.07@gmail.com>
2021-10-22 23:32:02 +0100

    Create form for changing hourly wage details

---
+++ app/src/main/java/com/incomecalculator/ChangeWageDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.incomecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangeWageDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_wage_details);
+    }
+}
--- app/src/main/java/com/incomecalculator/MainActivity.java
+++ app/src/main/java/com/incomecalculator/MainActivity.java
@@ -2,20 +2,52 @@ package com.incomecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.incomecalculator.db.DatabaseHelper;
 
 public class MainActivity extends AppCompatActivity {
 
-    public DatabaseHelper dbHelper;
+    private DatabaseHelper dbHelper;
+
+    private Button changeWageDetailsButton;
+    private Button addShiftButton;
+    private Button viewShiftsButton;
+    private Button calculateIncomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initializeDatabase();
+        initializeButtons();
+    }
+
+    public void launchChangeWageDetailsActivity(View view) {
+
+        Intent intent = new Intent(this, ChangeWageDetailsActivity.class);
+        startActivity(intent);
+    }
+
+    private void initializeButtons() {
+
+        changeWageDetailsButton = findViewById(R.id.change_wage_details_button);
+        addShiftButton = findViewById(R.id.add_shift_button);
+        viewShiftsButton = findViewById(R.id.view_shifts_button);
+        calculateIncomeButton = findViewById(R.id.calculate_income_button);
+
+        changeWageDetailsButton.setOnClickListener((view) -> {
+            launchChangeWageDetailsActivity(view);
+        });
+    }
+
+    private void initializeDatabase() {
+
         dbHelper = new DatabaseHelper(getApplicationContext());
-        dbHelper.getReadableDatabase();
+        dbHelper.getReadableDatabase().close();
     }
 }
